db1b6289f55f9ee68bf2c02a370930abc576324d
Mike LP <michael@portlandwebworks.com>
2014-09-29 17:04:59 -0400

    WIP: getting basic file structure

---
+++ dist/docs/js/docs-setup.js
@@ -0,0 +1,15 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "angular-cesiumjs-tpls-0.0.1-SNAPSHOT.js"
+  ]
+};
