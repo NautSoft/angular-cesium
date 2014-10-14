fef200f48f84a3334b94b7bce7f8ba1a2f487e21
Mike LP <michael@portlandwebworks.com>
2014-10-14 16:42:53 -0400

    WIP: Can now open a basic cesium viewer window.

--- src/viewer/viewer.js
+++ src/viewer/viewer.js
@@ -5,38 +5,50 @@
  */
 angular.module('cesiumjs.viewer', [])
 
-  .constant('viewerConfig', {
-    animation: true,
-    baseLayerPicker: true,
-    fullscreenButton: true,
-    geocoder:	true,
-    homeButton: true,
-    infoBox: true,
-    sceneModePicker: true,
-    selectionIndicator: true,
-    timeline: true,
-    navigationHelpButton: true,
-    navigationInstructionsInitiallyVisible: true,
-    scene3DOnly: false,
-    useDefaultRenderLoop: true,
-    targetFrameRate: 24,
-    showRenderLoopErrors: true,
-    automaticallyTrackDataSourceClocks: true
-  }).
+    .constant('viewerConfig', {
+        animation: true,
+        baseLayerPicker: true,
+        fullscreenButton: true,
+        geocoder:	true,
+        homeButton: true,
+        infoBox: true,
+        sceneModePicker: true,
+        selectionIndicator: true,
+        timeline: true,
+        navigationHelpButton: true,
+        navigationInstructionsInitiallyVisible: true,
+        scene3DOnly: false,
+        useDefaultRenderLoop: true,
+        targetFrameRate: 48,
+        showRenderLoopErrors: true,
+        automaticallyTrackDataSourceClocks: true
+    })
 
-  controller('ViewerController', function ($scope, $attrs, viewerConfig) {
-    $scope.cesiumViewer = null;
+    .controller('ViewerController', function($scope, $log, CesiumjsViewerService, viewerConfig) {
+        var service = CesiumjsViewerService;
+        $log.info('viewer controller init');
+        $log.info(viewerConfig);
+        $log.info(service);
+    })
 
-  }).
+    .service('CesiumjsViewerService', function ($log) {
+        $log.info('Service Init');
+        this.viewer = null;
+    })
 
-  directive('cesiumvViewer', function () {
-    return {
-      restrict: 'EA',
-      controller: 'ViewerController',
-      transclude: true,
-      replace: false,
-      templateUrl: 'template/accordion/viewer.html'
-    };
-  });
+    .directive('cesiumViewer', function ($log, viewerConfig) {
+        $log.info('cesiumViewer called' );
+        return {
+            restrict: 'EA',
+            controller: 'ViewerController',
+            transclude: true,
+            replace: false,
+            templateUrl: 'template/viewer/viewer.html',
+            link: function ($scope, element, attrs) {
+                $log.info('cesiumViewer');
+                $scope.cesiumViewer = new Cesium.Viewer(attrs.id, viewerConfig);
+            }
+        };
+    });
 
 
